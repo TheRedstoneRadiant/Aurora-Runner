77178c16917920defda219fca1f591fa672e28b8
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2022-12-28 02:35:52 -0500

    Execute docker containers

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import subprocess
 import pymongo
 import dotenv
 
+IMAGE_NAME = "aurora-selfbot"
+
 dotenv.load_dotenv()
 
 if not os.environ["MONGO_URI"]:
@@ -26,7 +28,19 @@ def add_token(DISCORD_TOKEN, CANVAS_TOKEN=""):
 
 def run_containers(tokens):
     for token in tokens:
-        print(token)
+        payload = [
+            "docker",
+            "run",
+            "-d",
+            "-e",
+            f"DISCORD_TOKEN={token['discordToken']}",
+        ]
+        if token["canvasToken"]:
+            payload += ["-e", f"CANVAS_TOKEN={token['canvasToken']}"]
+
+        payload.append(IMAGE_NAME)
+
+        subprocess.run(payload)
 
 
 if __name__ == "__main__":
