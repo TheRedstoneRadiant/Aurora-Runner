e0dcfaa8e8a0cbfc9afd7d51f71c09b5a29519d7
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2022-12-28 00:19:50 -0500

    Initialize boilerplate, add pymongo as a dependency

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import os
 import sys
 import subprocess
-import mongodb
+import pymongo
 import dotenv
 
-dotenv.load_dotenv()
 
-if not os.environ["MONGO_URI"]:
-    print("Missing MONGO_URI env variable")
-    sys.exit(1)
+def run_containers(tokens):
+    for token in tokens:
+        print(token)
+
+
+def main():
+    dotenv.load_dotenv()
+
+    if not os.environ["MONGO_URI"]:
+        print("Missing MONGO_URI env variable")
+        sys.exit(1)
+
+    db = pymongo.MongoClient(os.environ["MONGO_URI"])
+    token_collection = db.aurora.tokens
+
+    run_containers(token_collection.find())
+
+
+if __name__ == "__main__":
+    main()
